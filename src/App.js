461fc9e709858bfc85379145838d26494b3218fb
dindin-abdulloh <50541680+adin17-alt@users.noreply.github.com>
2021-06-20 17:55:55 +0700

    kondisional rendering

--- src/App.js
+++ src/App.js
@@ -7,20 +7,24 @@ class App extends React.Component{
   constructor(props){
     super(props)
 
-    this.state = {pengguna : Dummy}
+    this.state = {pengguna : Dummy, isLogin : true}
   }
 
     render(){
+      const isLoginclass = (this.state.isLogin) ? "sulogin" : "belogin";
+       
         return(
           <>
-            <div className="header">
-                <h1>Daftar Pengguna</h1>
+            <div className="header" >
+                <h1 className={isLoginclass}>Daftar Pengguna</h1>
             </div>
            <div>
            <ListUser users = {this.state.pengguna}/>
            </div>
           </>
         )
+       
+      
     }
 }
 
--- src/User.js
+++ src/User.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import './App.css'
-
+import Female from './img/girl.png'
+import Male from './img/man.png'
 const User = ({user}) => 
 (
+    <>
     <div className="user">
+        <div>
+            <img src={(user.gender === "male" ? Male : Female)} width="50px"/>
+        </div>
         Nama : {user.name} <br/>
         Usia : {user.age} <br/>
         Jenis Kelamin : {user.gender} <br/>
         Alamat : {user.address} <br/>
         Nama Perusahaan : {user.company}
+        
     </div>
+   
+</>
 )
 
 export default User
